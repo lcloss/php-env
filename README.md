b3120efed7c60476c73705aa2eb6e55d8ed6ff11
Luciano Closs <lcloss@gmail.com>
2020-10-16 23:34:03 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # php-env
 Simple Environment package
+
+## Requirements
+
+On root folder, create a .env file with information you need:
+
+```
+[database]
+driver = mysql
+server = localhost
+port   = 3306
+dbname = testdb
+user   = root
+password = 
+
+[some_section]
+key1    = value1
+key2 = "value3"
+
+[other_section]
+key1    = value2
+key2 = "value6"
+```
+
+## Usage
+
+### Retrieving a section
+
+```php
+use LCloss\Env\Environment;
+
+$path_from = '.' . DIRECTORY_SEPARATOR;
+
+$env = Environment::getInstance('.env', $path_from);
+$database = $env->database;
+
+// Or...
+$datbase = $env->getSection('database');
+```
+
+### Retrieving a property
+
+```php
+use LCloss\Env\Environment;
+
+$env = Environment::getInstance();  // Will retrieve .env file from current path
+$key1 = $env->key1; // Will retrive first property with this 'key1': value1
+
+// Or...
+$key1 = $env->getKey('other_section', 'key1');  // Retrieve: value2
+```
+
---
+++ src/Environment.php
@@ -0,0 +1,73 @@
+<?php
+namespace LCloss\Env;
+
+final class Environment
+{
+    private static $instance = NULL;
+    protected $base = "";
+    protected $env = [];
+
+    private function __construct() {}
+
+    public static function getInstance( $env_file = '.env', $path = '.' . DIRECTORY_SEPARATOR )
+    {
+        if ( is_null(self::$instance) ) {
+            self::$instance = new Environment();
+
+            $base = $path;
+            self::$instance->setBase( $base );
+
+            // Load initial data
+            self::$instance->load( $env_file );
+
+            // Setters with magic method
+            self::$instance->base_dir = $base;
+            self::$instance->env_file = $base . $env_file;
+        }
+
+        return self::$instance;
+    }
+
+    private function load( $env_file = '.env' )
+    {
+        $file = $this->base . $env_file;
+        if ( !file_exists($file) ) {
+            throw new \Exception(sprintf('File %s does not exists.', $file));
+        }
+
+        $this->env = parse_ini_file( $file, true );
+    }
+
+    public function getSection( $section )
+    {
+        return $this->env[ $section ];
+    }
+
+    public function getKey( $section, $key )
+    {
+        return $this->env[ $section ][ $key ];
+    }
+
+    public function __get( $key )
+    {
+        if ( array_key_exists( $key, $this->env ) ) {
+            return $this->env[ $key ];
+        } else {
+            foreach( $this->env as $section => $data ) {
+                if ( array_key_exists( $key, $data )) {
+                    return $data[ $key ];
+                }
+            }
+        }
+    }
+
+    public function __set( $key, $value ) 
+    {
+        $this->env[ $key ] = $value;
+    }
+
+    private function setBase( $base )
+    {
+        $this->base = $base;
+    }
+}
